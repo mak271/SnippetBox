8afdf342cea74b9120795494db71fca9f820a3fa
Mak271 <m.trofimov@involta.ru>
2022-05-27 12:32:38 +0300

    add first template

---
+++ cmd/web/handlers.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+ 
+func home(response http.ResponseWriter, request *http.Request) {
+    if request.URL.Path != "/" {
+        http.NotFound(response, request)
+        return
+    }
+
+	// Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
+	// файл home.page.tmpl должен быть *первым* файлом в срезе.
+	files := []string{
+		"./ui/html/home.page.html",
+		"./ui/html/base.layout.html",
+		"./ui/html/footer.partial.html",
+	}
+
+	// Используем функцию template.ParseFiles() для чтения файла шаблона.
+	// Если возникла ошибка, мы запишем детальное сообщение ошибки и
+	// используя функцию http.Error() мы отправим пользователю
+	// ответ: 500 Internal Server Error (Внутренняя ошибка на сервере)
+	template, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(response, "Internal Server Error", 500)
+	}
+
+	// Затем мы используем метод Execute() для записи содержимого
+	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
+	// возможность отправки динамических данных в шаблон.
+	err = template.Execute(response, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(response, "Internal Server Error", 500)
+	}
+}
+ 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    if err != nil || id < 1 {
+        http.NotFound(w, r)
+        return
+    }
+ 
+    fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
+}
+ 
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        w.Header().Set("Allow", http.MethodPost)
+        http.Error(w, "Метод не дозволен", http.StatusMethodNotAllowed)
+        return
+    }
+ 
+    w.Write([]byte("Создание новой заметки..."))
+}
